19c8c864a501d3353dd5ece37f8888567b951ea1
ZEESHAN0603 <06zeeshan03@gmail.com>
2026-08-09 20:37:30 +0530

    ADD 1

---
+++ Phase1/data_type/Non_Primitive_type/string_cls.java
@@ -0,0 +1,19 @@
+package data_type.Non_Primitive_type;
+
+// String cls called using obj on the main method
+public class string_cls {
+    String StudentName;
+    int StudentRegNo;
+    int StudentAge;
+
+    public static void main(String[] args) {
+        string_cls obj = new string_cls();
+        obj.StudentName = "Zeeshan";
+        obj.StudentRegNo = 12345;
+        obj.StudentAge = 20;
+
+        System.out.println("Student Name: " + obj.StudentName);
+        System.out.println("Student Registration Number: " + obj.StudentRegNo);
+        System.out.println("Student Age: " + obj.StudentAge);
+    }
+}
